398dafa5810968190863c4cd4002ed6c5c656476
Callum Axon <callumaxon@gmail.com>
2019-02-27 19:59:02 +0000

    Ignore CMake build files

--- tests/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/callumaxon/Programming/cpp/sdi-project/tests
-# Build directory: /Users/callumaxon/Programming/cpp/sdi-project/tests/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
