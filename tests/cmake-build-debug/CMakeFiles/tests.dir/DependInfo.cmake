e4d8548ed4cfcd53f54641d9fa4fe1fda32b150c
Callum Axon <callumaxon@gmail.com>
2019-02-27 19:55:39 +0000

    Initial Commit w/ GTest Integration

---
+++ tests/cmake-build-debug/CMakeFiles/tests.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/callumaxon/Programming/cpp/sdi-project/tests/exampleTest.cpp" "/Users/callumaxon/Programming/cpp/sdi-project/tests/cmake-build-debug/CMakeFiles/tests.dir/exampleTest.cpp.o"
+  "/Users/callumaxon/Programming/cpp/sdi-project/tests/tests.cpp" "/Users/callumaxon/Programming/cpp/sdi-project/tests/cmake-build-debug/CMakeFiles/tests.dir/tests.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/usr/local/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ tests/exampleTest.cpp
@@ -0,0 +1,15 @@
+#include "gtest/gtest.h"
+
+namespace TrekStarTest {
+    namespace Example {
+        struct Str {
+
+        };
+
+
+        TEST(Str, ExampleTest) {
+            ASSERT_EQ(1, 1);
+        }
+    }
+}
+
