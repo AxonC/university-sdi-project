f14f71b98ebff2156c941b2df03f2dd316b9c58b
Matthew Robinson <matt@00n.me>
2019-03-16 17:04:56 +0000

    Added basic command input.

--- src/trekstar.cpp
+++ src/trekstar.cpp
@@ -4,5 +4,16 @@ int main()
 {
     std::cout << "Welcome to TrekStar Management System | Console" << std::endl;
 
+    std::string command = "";
+
+    while ( command != "q" && command != "quit" )
+    {
+        std::cout << "Available commands: " << std::endl;
+        std::cout << " - list projects (or lsp)" << std::endl;
+        std::cout << " - quit          (or q)" << std::endl;
+
+        std::cin >> command;
+    }
+
     return 0;
 }
