572e206285a17a20fdc8f4fe5328f1f2cd957c82
Matthew Robinson <matt@00n.me>
2019-04-11 16:21:38 +0100

    Added DVDSide class.

---
+++ src/material/DVDSide.cpp
@@ -0,0 +1,13 @@
+#ifndef TREKSTAR_DOUBLESIDEDVD_H
+#define TREKSTAR_DOUBLESIDEDVD_H
+
+#include "DVDSide.h"
+
+namespace TrekStar {
+    namespace Material {
+
+    }
+}
+
+
+#endif //TREKSTAR_DOUBLESIDEDVD_H
---
+++ src/material/DVDSide.h
@@ -0,0 +1,16 @@
+#ifndef TREKSTAR_DOUBLESIDEDVD_H
+#define TREKSTAR_DOUBLESIDEDVD_H
+
+#include "DVD.h"
+
+namespace TrekStar {
+    namespace Material {
+        class DVDSide{
+        public:
+            DVDSide() = default;
+        };
+    }
+}
+
+
+#endif //TREKSTAR_DOUBLESIDEDVD_H
