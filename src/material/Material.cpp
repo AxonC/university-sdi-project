6c2ccf0c70890dde2ecefe145849d247671c3546
Matthew Robinson <matt@00n.me>
2019-04-12 16:01:09 +0100

    Removed unused constructor and removed ID from GetPresentableInformation.

--- src/material/Material.cpp
+++ src/material/Material.cpp
@@ -10,31 +10,6 @@ namespace TrekStar {
         /**
           *  Trekstar Material Class Implementation
         */
-        Material::Material(std::string line)
-        {
-//            std::vector<std::string> materialAttributes;
-//
-//            std::string::size_type pos = line.find('|');
-//            line = line.substr(pos + 1);
-//
-//            while (pos != std::string::npos)
-//            {
-//                pos = line.find('|');
-//                materialAttributes.push_back(line.substr(0, pos));
-//                line = line.substr(pos + 1);
-//            }
-//
-//            this->id = static_cast<unsigned int>(std::stoi(materialAttributes[0]));
-//            this->title = materialAttributes[1];
-//            this->format = materialAttributes[2];
-//            this->audioFormat = materialAttributes[3];
-//            this->runTime = materialAttributes[4];
-//            this->language = materialAttributes[5];
-//            this->retailPrice = std::stoul(materialAttributes[6], nullptr ,0);
-//            this->subtitles = materialAttributes[7];
-//            this->frameAspect = materialAttributes[8];
-        }
-
         Material::Material(unsigned int id, const std::string & title)
         {
             this->id = id;
@@ -100,7 +75,6 @@ namespace TrekStar {
         {
             return
                 {
-                    {"ID", std::to_string(this->GetId())},
                     {"Format", this->GetFormat()},
                     {"Audio Format", this->GetAudioFormat()},
                     {"Run Time", std::to_string(this->GetRunTime())},
