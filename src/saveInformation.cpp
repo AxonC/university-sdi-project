05fc381e9e122b86f0bedab85d2cd6818bd81622
Matthew Robinson <matt@00n.me>
2019-04-15 11:28:39 +0100

    Fixed hierarchy of JSON export and now iterates through projects.

--- src/saveInformation.cpp
+++ src/saveInformation.cpp
@@ -21,37 +21,40 @@ void save(std::vector<TrekStar::Project::Project> projects, std::string filePath
         counter++;
         json currentProjectJSON;
 
-        currentProjectJSON["details"] = TrekStar::Project::to_json(projects.at(0).ExportToSerialised());
+        currentProjectJSON["details"] = TrekStar::Project::to_json(project.ExportToSerialised());
+
+        toWriteJSON[std::to_string(counter)] = currentProjectJSON;
+
+
+        json materialArray;
+        for (const auto & m: project.GetMaterials())
+        {
+            if( auto materialType = std::dynamic_pointer_cast<TrekStar::Material::DoubleSideDVD>(m) )
+            {
+                materialArray.push_back(TrekStar::Material::to_json(materialType->ExportToSerialised(), m));
+            }
+            else if( auto materialType = std::dynamic_pointer_cast<TrekStar::Material::DVD>(m) )
+            {
+                materialArray.push_back(TrekStar::Material::to_json(materialType->ExportToSerialised(), m));
+            }
+            else
+            {
+                materialArray.push_back(TrekStar::Material::to_json(m->ExportToSerialised()));
+            }
+        }
+
+        currentProjectJSON["materials"] = materialArray;
+
+        json crewArray;
+        for (const auto & crew: project.GetCrew())
+        {
+            crewArray.push_back(TrekStar::People::to_json(crew->ExportToSerialized()));
+        }
+
+        currentProjectJSON["crew"] = crewArray;
 
         toWriteJSON[std::to_string(counter)] = currentProjectJSON;
     }
-
-    json materialArray;
-    for (const auto & m: projects.at(0).GetMaterials())
-    {
-        if( auto materialType = std::dynamic_pointer_cast<TrekStar::Material::DoubleSideDVD>(m) )
-        {
-            materialArray.push_back(TrekStar::Material::to_json(materialType->ExportToSerialised(), m));
-        }
-        else if( auto materialType = std::dynamic_pointer_cast<TrekStar::Material::DVD>(m) )
-        {
-            materialArray.push_back(TrekStar::Material::to_json(materialType->ExportToSerialised(), m));
-        }
-        else
-        {
-            materialArray.push_back(TrekStar::Material::to_json(m->ExportToSerialised()));
-        }
-    }
-
-    toWriteJSON["materials"] = materialArray;
-
-    json crewArray;
-    for (const auto & crew: projects.at(0).GetCrew())
-    {
-        crewArray.push_back(TrekStar::People::to_json(crew->ExportToSerialized()));
-    }
-
-    toWriteJSON["crew"] = crewArray;
 
     std::ofstream JSONfile(filePath);
     JSONfile << std::setw(4) << toWriteJSON << std::endl;
