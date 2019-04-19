ba4deb1631f7bb18fabe79e592e204664f11753f
Matthew Robinson <matt@00n.me>
2019-04-19 16:05:46 +0100

    Implemented Material view and controller; refactored "lsm" command.

---
+++ src/view/material/MaterialView.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Matt Robinson on 2019-04-19.
+//
+
+#include "MaterialView.h"
---
+++ src/view/material/MaterialView.h
@@ -0,0 +1,12 @@
+//
+// Created by Matt Robinson on 2019-04-19.
+//
+
+#ifndef TREKSTAR_MATERIALVIEW_H
+#define TREKSTAR_MATERIALVIEW_H
+
+class MaterialView {
+
+};
+
+#endif //TREKSTAR_MATERIALVIEW_H
