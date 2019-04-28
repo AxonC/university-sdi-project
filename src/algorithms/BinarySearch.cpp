822d6feb66cb6033bf95c3ed5a666714262f0b06
Matthew Robinson <matt@00n.me>
2019-04-28 12:23:48 +0100

    Documented the BinarySearch algorithm.

--- src/algorithms/BinarySearch.cpp
+++ src/algorithms/BinarySearch.cpp
@@ -4,26 +4,34 @@ namespace TrekStar
 {
     namespace Algorithms
     {
+        /**
+          Function Name: binarySearch
+
+          Responsible for performing binary search on a vector of Projects.
+
+          @param The index of the target Project, or -1 if the target could not be found.
+        */
         unsigned int binarySearch(std::vector<TrekStar::Project::Project> & list, std::string target)
         {
-            int result = -1;
-            int low = 0;
-            int high = list.size() - 1;
+            int result = -1;             // Set result to not found.
+            int low = 0;                 // Set low to the index of the first Project in the vector.
+            int high = list.size() - 1;  // Set high to the index of the last Project in the vector.
             int mid;
 
+            // While the result is not found and not all items have been checked.
             while ( result == -1 && low <= high )
             {
                 mid = low + ((high - low) / 2);
 
-                if ( list[mid] == target )
+                if ( list[mid] == target )  // If the middle item is the target.
                 {
                     result = mid;
                 }
-                else if( list[mid] < target )
+                else if ( list[mid] < target )  // If the target is on the left.
                 {
                     low = mid + 1;
                 }
-                else
+                else  // If the target is on the right.
                 {
                     high = mid - 1;
                 }
